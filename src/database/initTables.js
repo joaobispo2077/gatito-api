5397d902bb2abca9e2511d166383c2e0ff6cfcfb
João Bispo <vitor.vi10@hotmail.com>
2021-02-20 15:05:23 -0300

    feat: added products table

--- src/database/initTables.js
+++ src/database/initTables.js
@@ -1,6 +1,17 @@
 const tableProviders = require('../routes/providers/modelTableProviders');
+const tableProducts = require('../routes/providers/products/modelTableProduct');
 
-tableProviders
-    .sync()
-    .then(() => console.log('Tabela de fornecedores criada com sucesso'))
-    .catch(console.log);
+const tables = [
+  tableProviders,
+  tableProducts
+]
+async function createTables() {
+  await tables.forEach(async table => {
+    await table
+      .sync()
+      .then(() => console.log(`Tabela ${table} criada com sucesso`))
+      .catch(console.log);
+  });
+}
+
+createTables();
---
+++ src/routes/providers/products/modelTableProduct.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+const connection = require('../../../database/connection');
+
+const columns = {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  stock: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  provider: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: require('../modelTableProviders'),
+      key: 'id'
+    }
+  }
+}
+
+const options = {
+  freezeTableName: true,
+  tableName: 'product',
+  timestamps: true
+}
+
+module.exports = connection.define('product', columns, options);
