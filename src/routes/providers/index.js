19842ae5f001b4eb4b25abd99137df23e8c9b61e
João Bispo <vitor.vi10@hotmail.com>
2021-02-20 14:46:15 -0300

    feat: add route to list products of a provider

--- src/routes/providers/index.js
+++ src/routes/providers/index.js
@@ -5,81 +5,85 @@ const Provider = require('./Provider');
 const SerializerProvider = require('../../Serializer').SerializerProvider;
 
 router.get('/', async(req, res) => {
-    const providers = await TableProvider.listAll();
+  const providers = await TableProvider.listAll();
 
 
-    const serializeProvider = new SerializerProvider(
-        res.getHeader('Content-Type')
-    );
+  const serializeProvider = new SerializerProvider(
+    res.getHeader('Content-Type')
+  );
 
-    res.status(200); //.json(providers);
-    res.send(serializeProvider.serialize(providers));
+  res.status(200); //.json(providers);
+  res.send(serializeProvider.serialize(providers));
 });
 
 router.get('/:id', async(req, res, next) => {
 
-    try {
-        const id = req.params.id;
-        const provider = new Provider({ id: id });
+  try {
+    const id = req.params.id;
+    const provider = new Provider({ id: id });
 
-        await provider.findById();
-        res.status(200); // .json(provider);
-        const serializeProvider = new SerializerProvider(
-            res.getHeader('Content-Type'), ['email', 'dataCriacao', 'dataAtualizacao', 'versao']
-        );
+    await provider.findById();
+    res.status(200); // .json(provider);
+    const serializeProvider = new SerializerProvider(
+      res.getHeader('Content-Type'), ['email', 'dataCriacao', 'dataAtualizacao', 'versao']
+    );
 
-        res.send(serializeProvider.serialize(provider));
+    res.send(serializeProvider.serialize(provider));
 
-    } catch (err) {
-        next(err);
-    }
+  } catch (err) {
+    next(err);
+  }
 
 });
 
 router.post('/', async(req, res, next) => {
-    try {
-        const data = req.body;
-        const provider = new Provider(data);
-        const createdProvider = await provider.create();
+  try {
+    const data = req.body;
+    const provider = new Provider(data);
+    const createdProvider = await provider.create();
 
 
 
-        const serializeProvider = new SerializerProvider(
-            res.getHeader('Content-Type')
-        );
+    const serializeProvider = new SerializerProvider(
+      res.getHeader('Content-Type')
+    );
 
-        res.status(201) //.json(createdProvider);
-            .send(serializeProvider.serialize(provider));
+    res.status(201) //.json(createdProvider);
+      .send(serializeProvider.serialize(provider));
 
-    } catch (err) {
-        next(err);
-    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.patch('/:id', async(req, res, next) => {
-    try {
-        const id = req.params.id;
-        const data = req.body;
-        const datas = Object.assign({}, data, { id: id });
-        const provider = new Provider(datas);
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const datas = Object.assign({}, data, { id: id });
+    const provider = new Provider(datas);
 
-        await provider.update();
-        res.status(204).end();
-    } catch (err) {
-        next(err);
-    }
+    await provider.update();
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.delete('/:id', async(req, res, next) => {
-    try {
-        const id = req.params.id;
-        const provider = new Provider({ id: parseInt(id) });
-        await provider.delete();
+  try {
+    const id = req.params.id;
+    const provider = new Provider({ id: parseInt(id) });
+    await provider.delete();
 
-        res.status(200).json({ id: id });
-    } catch (err) {
-        next(err);
-    }
+    res.status(200).json({ id: id });
+  } catch (err) {
+    next(err);
+  }
 });
+
+const productsRouter = require('./products');
+
+router.use('/:id/products', productsRouter);
 
 module.exports = router;
